ae2c345cfbb4461bb05eed4085c132f1750d8414
Denny <dev.dennysegura@gmail.com>
2019-03-18 13:34:31 -0300

    release 0.0.5

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:wearable_data_layer/wearable_data_layer.dart';
 
+final MethodChannel channel = MethodChannel("wearable_data_layer");
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -12,43 +14,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
+  Future<String> _message = Future<String>.value('');
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await WearableDataLayer.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+  void assignFuture(Function func) {
     setState(() {
-      _platformVersion = platformVersion;
+      _message = func();
     });
+  }
+
+  Future<String> sendMessage() async {
+    final bool response = await WearableDataLayer.sendDefaultPath(
+      'Saludos desde wearable',
+    );
+    return '''path:
+    /flutter_wearable_datalayer: 
+    response: $response''';
+  }
+
+  Future<String> sendCustomPathMessage() async {
+    final bool response = await WearableDataLayer.sendCustomPath(
+      'test',
+      'Saludos desde wearable custom',
+    );
+    return '''path:
+    /test: 
+    response: $response''';
+  }
+
+  void receivedMessage(dynamic data) {
+    print('data received on mobile: $data');
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: WearableReceiverWidget(
+            builder: (context, data) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Mensaje: ${data != null ? data.toString() : ''}',
+                  ),
+                  MaterialButton(
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    child: Text('Send Default'),
+                    onPressed: () => assignFuture(this.sendMessage),
+                  ),
+                  MaterialButton(
+                    color: Colors.indigo,
+                    textColor: Colors.white,
+                    child: Text('Send Custom'),
+                    onPressed: () => assignFuture(this.sendCustomPathMessage),
+                  ),
+                  FutureBuilder<String>(
+                      future: _message,
+                      builder: (_, AsyncSnapshot<String> snapshot) {
+                        return Text(
+                          snapshot.data ?? '',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        );
+                      }),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
